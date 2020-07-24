82b4127d59cb7e3aff18e3385db74bd7a70e2b31
rafael <rafael@rafael-Aspire-E5-571>
2020-07-24 17:53:43 -0300

    Commit inicial

---
+++ src/br/edu/ifsul/modelo/Pais.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifsul.modelo;
+
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+/**
+ *
+ * @author rafael
+ */
+@Entity
+public class Pais implements Serializable{
+   @Id
+   @SequenceGenerator(name="seq_pais",sequenceName="seq_pais_id",allocationSize=1)
+   @GeneratedValue(generator = "seq_pais",strategy = GenerationType.SEQUENCE)
+   private Integer id;
+   @Column(name="nome",nullable=false,length = 50)
+   private String nome;
+   @Column(name = "iso",nullable = false,length = 3) 
+   private String iso;
+
+    /**
+     * @return the id
+     */
+    
+    public Pais(){
+    
+    }
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the iso
+     */
+    public String getIso() {
+        return iso;
+    }
+
+    /**
+     * @param iso the iso to set
+     */
+    public void setIso(String iso) {
+        this.iso = iso;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 13 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Pais other = (Pais) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+}
