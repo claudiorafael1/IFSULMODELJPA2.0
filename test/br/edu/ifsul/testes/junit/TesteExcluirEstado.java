9e59044b30fa95d8f559ee307b64316c0fb5bfb0
rafael <rafael@rafael-Aspire-E5-571>
2020-07-25 17:08:58 -0300

    Utilizando o Junit

---
+++ test/br/edu/ifsul/testes/junit/TesteExcluirEstado.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifsul.testes.junit;
+
+import br.edu.ifsul.jpa.EntityManagerUtil;
+import br.edu.ifsul.modelo.Estado;
+import br.edu.ifsul.modelo.Pais;
+import javax.persistence.EntityManager;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author rafael
+ */
+public class TesteExcluirEstado {
+       EntityManager em;
+    public TesteExcluirEstado() {
+    }
+    
+    @Before
+    public void setUp() {
+        em=EntityManagerUtil.getEntityMenager();
+    }
+    
+    @After
+    public void tearDown() {
+        em.close();
+    }
+    
+    @Test
+      public void teste(){
+   
+
+    boolean exception =false;
+    try{
+  
+        
+        
+     Estado p = em.find(Estado.class,7);
+       em.getTransaction().begin();
+       em.remove(p);
+       em.getTransaction().commit();
+        }catch (Exception e ){
+        exception=true;
+        e.printStackTrace();
+        }
+    Assert .assertEquals(false, exception);
+    }
+    
+}
